c55b0fc35debf7ac107358bbe0d96ef5a66a682f
Pitipat Srichairat <pitipat.dop@gmail.com>
2017-06-26 11:38:41 +0700

    move webui

---
+++ webui/src/containers/Header/index.js
@@ -0,0 +1,116 @@
+import React, { Component } from 'react';
+import { Menu, Dropdown, Icon } from 'antd';
+import { NavLink } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { withTranslate, IntlActions } from 'react-redux-multilingual';
+import FontAwesome from 'react-fontawesome';
+import logo from '../../images/logo.png';
+import * as firebase from '../../api/firebase';
+
+class Header extends Component {
+
+	state = {
+		user: {},
+		showMobileMenu: false,
+	}
+
+	componentDidMount() {
+		const _self = this;
+		firebase.core().auth().onAuthStateChanged(function(user) {
+		  if (user && user.emailVerified === true) {
+		  	_self.setState(prevState => ({
+		      user: user,
+		    }));
+		  }
+		});
+	}
+
+	handleMobileMenu = () => {
+		this.setState(prevState => ({
+      showMobileMenu: !prevState.showMobileMenu,
+    }));
+	}
+
+	logout = () => {
+		const _self = this;
+		firebase.core().auth().signOut().then(function() {
+		  _self.setState(prevState => ({
+	      user: {},
+	    }));
+		}).catch(function(error) {
+		  console.log('error', error);
+		});
+	}
+
+	handleMenuClick = (value) => {
+		if ( value.key === 'logout' ) {
+			this.logout();
+		}
+	}
+
+  render() {
+
+  	const { user, showMobileMenu } = this.state;
+  	const { translate, dispatch } = this.props;
+
+  	let loginLabel = 
+  										<NavLink exact to="/login">
+				            		<FontAwesome name='user-o' />
+				            		{translate('เข้าสู่ระบบ')}
+				            	</NavLink>
+
+		if ( user.email ) {
+			const menu = (
+			  <Menu onClick={this.handleMenuClick}>
+			    <Menu.Item key="profile">
+			    	<NavLink exact to="/">Profile</NavLink>
+			    </Menu.Item>
+			    <Menu.Divider />
+			    <Menu.Item key="logout">Logout</Menu.Item>
+			  </Menu>
+			);
+			loginLabel = 	
+									<Dropdown overlay={menu} trigger={['click']}>
+								    <a className="ant-dropdown-link" href="#">
+								      {user.email} <Icon type="down" />
+								    </a>
+								  </Dropdown>
+		}
+
+    return (
+      <nav id="Header" className="navbar navbar-default navbar-fixed-top">
+	      <div className="container">
+	        <div className="navbar-header">
+	          <button type="button" className={"navbar-toggle " + (showMobileMenu ? '' : 'collapsed')} data-toggle="collapse" data-target="#navbar" aria-expanded={showMobileMenu ? 'true' : 'false'} aria-controls="navbar" onClick={this.handleMobileMenu}>
+	            <span className="sr-only">Toggle navigation</span>
+	            <span className="icon-bar"></span>
+	            <span className="icon-bar"></span>
+	            <span className="icon-bar"></span>
+	          </button>
+	          <NavLink className="navbar-brand" exact to="/">
+	          	<img src={logo} alt="PropShop" />
+	          </NavLink>
+	        </div>
+	        <div id="navbar" className={"navbar-collapse collapse " + (showMobileMenu ? 'in' : '')}>
+	          <ul className="nav navbar-nav">
+	            <li><NavLink exact to="/">{translate('ต้องการซื้อ - เช่า')}</NavLink></li>
+	            <li><NavLink exact to="/sell">{translate('ประกาศขาย - เช่า')}</NavLink></li>
+	            <li><NavLink exact to="/agent">{translate('ค้นหานายหน้า')}</NavLink></li>
+	            <li><NavLink exact to="/news">{translate('ข่าวสารและบทความ')}</NavLink></li>
+	            <li><NavLink exact to="/webboard">{translate('เว็บบอร์ด')}</NavLink></li>
+	          </ul>
+	          <ul className="nav navbar-nav navbar-right">
+	            <li className="language"><a onClick={(e) => { dispatch(IntlActions.setLocale('th')) }}>ไทย</a></li>
+	            <li className="language last"><a onClick={(e) => { dispatch(IntlActions.setLocale('en')) }}>ENG</a></li>
+	            <li className="login">
+	            	{loginLabel}
+	            </li>
+	          </ul>
+	        </div>
+	      </div>
+	    </nav>
+    );
+  }
+}
+
+export default connect()(withTranslate(Header));
---
+++ webui/src/pages/Forgotpassword/index.js
@@ -0,0 +1,171 @@
+import React, { Component } from 'react';
+import { Input, Alert, Spin } from 'antd';
+import { NavLink, Redirect } from 'react-router-dom';
+import FontAwesome from 'react-fontawesome';
+
+import * as firebase from '../../api/firebase';
+import * as helpers from '../../helpers';
+
+import BannerRealEstate from '../../containers/BannerRealEstate';
+import SocialLogin from '../../containers/SocialLogin';
+
+class Forgotpassword extends Component {
+
+  state = {
+    loading: false,
+    submit: false,
+    errorMessage: '',
+    email: {
+      value: '',
+      errorMessage: '',
+    },
+  }
+
+  componentDidMount() {
+    const { history } = this.props;
+    firebase.core().auth().onAuthStateChanged(function(user) {
+      if (user && user.emailVerified === true) {
+        history.push({
+          pathname: '/',
+        });
+      }
+    });
+  }
+
+  handleInputEmail = (e) => {
+    const value = e.target.value;
+    this.setState(prevState => ({
+      email: {
+        ...prevState.email,
+        value: value,
+      }
+    }));
+  }
+
+  checkEmail = (email) => {
+    const errorMessage = helpers.errorMessageInputEmail(email);
+    this.setState(prevState => ({
+      email: {
+        ...prevState.email,
+        errorMessage: errorMessage,
+      }
+    }));
+    return errorMessage;
+  }
+
+  submit = () => {
+    const { loading } = this.state;
+
+    if ( loading === true ) {
+      return;
+    } else {
+      this.setState({
+        loading: true,
+      });
+    }
+
+    const _self = this;
+
+    const email = this.state.email.value;
+
+    const errorEmail = this.checkEmail(email);
+
+    if ( errorEmail === '' ) {
+      firebase.forgotpassword(email).then(function(errorMessage) {
+        if ( errorMessage ) {
+          _self.setState({
+            loading: false,
+            errorMessage: errorMessage,
+          });
+        } else {
+          _self.setState({
+            submit: true,
+            loading: false,
+            errorMessage: '',
+          });
+        }
+      });
+    } else {
+      this.setState({
+        loading: false,
+      });
+    }
+  }
+
+  handleSocialError = (error) => {
+    if (  error.type === 'auth/account-exists-with-different-credential' ) {
+      this.setState({
+        errorMessage: error.message,
+        email: {
+          ...this.state.email,
+          value: error.email,
+        }
+      });
+    }
+  }
+
+  render() {
+
+    const { loading, submit } = this.state;
+
+    const emailErrorMessage = this.state.email.errorMessage ? <span className="text-red">({this.state.email.errorMessage})</span> : '';
+
+    const form = 
+          <div>
+            <div className="form">
+              <div className="row">
+                <div className="col-sm-8 col-sm-offset-2 col-md-8 col-md-offset-2 col-lg-6 col-lg-offset-3">
+                  <h1>ลืมรหัสผ่าน</h1>
+                  {this.state.errorMessage !== '' &&
+                    <div className="form-group">
+                      <Alert message={this.state.errorMessage} type="error" />
+                    </div>
+                  }
+                  {submit === true ? (
+                    <div className="form-group">
+                      <label>กรุณาตรวจสอบอีเมล {this.state.email.value}</label>
+                    </div>
+                  ) : (
+                    <div>
+                      <div className="form-group">
+                        <label>อีเมล {emailErrorMessage}</label>
+                        <Input onChange={this.handleInputEmail} value={this.state.email.value} />
+                      </div>
+                      <div className="form-group action">
+                        <button className="btn btn-primary" onClick={this.submit} >ลืมรหัสผ่าน</button>
+                      </div>
+                    </div>
+                  )}
+                </div>
+              </div>
+            </div>
+            <hr/>
+            <div className="social_login">
+              <SocialLogin error={this.handleSocialError} />
+            </div>
+          </div>
+
+    return (
+      <div id="Forgotpassword">
+        <div className="row">
+          <div className="hidden-xs hidden-sm col-md-6 layout-left">
+            <BannerRealEstate />
+          </div>
+          <div className="col-md-6 col-md-offset-6 layout-right">
+            {loading === true ? (
+              <Spin tip="Loading...">
+                {form}
+              </Spin>
+            ) : (
+              <div>
+                {form}
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Forgotpassword;
